90471aa4e60060d86444bddae80c7af31d6af290
abhsihek <abhishekshakya98.com@gmail.com>
2024-02-09 12:07:19 +0545

    removed the nested folder from recent bookmark service

--- src/app/services/recentbookmark.service.ts
+++ src/app/services/recentbookmark.service.ts
@@ -215,21 +215,6 @@ export class recentBookmarkService {
     },
   ]
 
-  private nestedFolder: Folder[] = [
-    {
-      id: 1,
-      title: "Design Inspiration"
-    },
-    {
-      id: 2,
-      title: "Dribble"
-    },
-    {
-      id: 3,
-      title: "Mobbin"
-    }
-  ]
-
   recentBookmarkObservable = new BehaviorSubject<Recentbookmark[]>(this.recentBookmark);
   
   constructor(private stateService:StateService) { }
@@ -318,28 +303,5 @@ export class recentBookmarkService {
         this.recentBookmarkObservable.next(filteredBookmarks);
       })
     ).subscribe();
-  }
- 
-  // Nested Folder
-  getNestedFolder() {
-    const sortedData: Folder[] = this.nestedFolder.sort((a, b): number => {
-
-      // If both have the same folder property, sort alphabetically by title
-      const titleA = a.title || ''; // Use an empty string if a.title is undefined
-      const titleB = b.title || ''; // Use an empty string if b.title is undefined
-
-      return titleA.localeCompare(titleB);
-    });
-
-    return sortedData;
-  }
-
-  addNestedFolder(name: string) {
-    this.nestedFolder.push({ id: this.nestedFolder.length + 1, title: name })
-    this.stateService.state.folderInputbox = false;
-  }
-
-  deleteNestedFolder(id: number) {
-    this.nestedFolder = this.nestedFolder.filter((item) => item.id !== id)
   }
 }
