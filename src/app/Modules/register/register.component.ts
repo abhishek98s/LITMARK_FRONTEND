93a7a42dd7043ee185c3c04208e591206be96afe
abhishek98s <abhishekshakya98.com@gmail.com>
2024-12-25 11:09:43 +0545

    add: toggle password and register

--- src/app/Modules/register/register.component.ts
+++ src/app/Modules/register/register.component.ts
@@ -7,38 +7,49 @@ import { ToastService } from 'src/app/services/toast.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  isPasswordShown: boolean = false;
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router, private toast: ToastService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toast: ToastService
+  ) {
     this.registerForm = new FormGroup({
       username: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)])
-    })
+      password: new FormControl('', [
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/),
+      ]),
+    });
   }
-
 
   submitForm() {
     if (!(this.registerForm.status === 'VALID' && !this.registerForm.errors)) {
-      this.toast.error('All field are required.')
-      return
+      this.toast.error('All field are required.');
+      return;
     }
     this.authService.onRegister(this.registerForm.value).subscribe({
       next: () => {
-        this.router.navigate(['/login'])
+        this.router.navigate(['/login']);
         this.toast.success('You are registered.');
       },
       error: (error) => {
         const err = error.error.msg;
         if (!err) {
-          this.toast.error("Check connection.");
-          return
+          this.toast.error('Check connection.');
+          return;
         }
-        this.toast.error(err)
-      }
-    })
+        this.toast.error(err);
+      },
+    });
+  }
+
+  togglePassword() {
+    this.isPasswordShown = !this.isPasswordShown;
   }
 }
