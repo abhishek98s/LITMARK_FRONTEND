db42e4286dfdd0006f65f1f50c1a43382bbb9b85
abhishek98s <abhishekshakya98.com@gmail.com>
2025-01-21 13:01:51 +0545

    fix: repetive folder click

--- src/app/Shared/sidebarfolder-searched-item/folder-search-item.component.ts
+++ src/app/Shared/sidebarfolder-searched-item/folder-search-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { SidebarFolder } from 'src/app/Model/sidebarFolder.model';
 import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
 import { dropDownService } from 'src/app/services/dropdown.service';
@@ -8,19 +9,29 @@ import { sidebarFolderService } from 'src/app/services/sidebarFolder.service';
 @Component({
   selector: 'app-sidebarfolder-searched-item',
   templateUrl: './sidebarfolder-searched-item.component.html',
-  styleUrls: ['./sidebarfolder-searched-item.component.scss']
+  styleUrls: ['./sidebarfolder-searched-item.component.scss'],
 })
 export class FolderSearchItemComponent {
   @Input() folder!: SidebarFolder;
 
-  constructor(private dropDownService: dropDownService, private searchTextService: SearchTextService, private sidebarFolderService: sidebarFolderService, private breadcrumbService: BreadcrumbService) { }
+  constructor(
+    private dropDownService: dropDownService,
+    private searchTextService: SearchTextService,
+    private sidebarFolderService: sidebarFolderService,
+    private breadcrumbService: BreadcrumbService,
+    private router: Router
+  ) {}
 
   onFolderSearchClick() {
     this.dropDownService.clearDropdowns();
-    this.searchTextService.clearSearchText()
+    this.searchTextService.clearSearchText();
 
+    this.sidebarFolderService.setFolderActive(this.folder.id);
+    this.breadcrumbService.setinitialBreadcrumbs(
+      this.folder.name,
+      this.folder.id
+    );
 
-    this.sidebarFolderService.setFolderActive(this.folder.id)
-    this.breadcrumbService.setinitialBreadcrumbs(this.folder.name, this.folder.id)
+    this.router.navigate([`bookmark/${this.folder.id}`]);
   }
 }
