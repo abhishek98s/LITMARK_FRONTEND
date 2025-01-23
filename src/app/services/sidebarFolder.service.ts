4f0b499dead7bcaf91b61904c7c58c48d86b21ac
abhishek98s <abhishekshakya98.com@gmail.com>
2025-01-23 17:08:40 +0545

    fix: import

--- src/app/services/sidebarFolder.service.ts
+++ src/app/services/sidebarFolder.service.ts
@@ -1,35 +1,43 @@
 import { Injectable, WritableSignal, signal } from '@angular/core';
-import { SidebarFolder, SidebarFolderApiBody, SidebarFolderArrayResponse, SidebarFolderResponse } from '../model/sidebarFolder.model';
-import { BehaviorSubject, forkJoin, map, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
+import {
+  SidebarFolder,
+  SidebarFolderApiBody,
+  SidebarFolderArrayResponse,
+  SidebarFolderResponse,
+} from '../model/sidebarFolder.model';
+import { map } from 'rxjs';
 import { ToastService } from './toast.service';
 import { dropDownService } from './dropdown.service';
 import { APP_URL } from '../utils/app.config';
-
 
 interface UpdateFolderBody {
   name: string;
 }
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class sidebarFolderService {
-  private sidebarFolders: WritableSignal<SidebarFolder[]> = signal([])
+  private sidebarFolders: WritableSignal<SidebarFolder[]> = signal([]);
 
   private searchResult: WritableSignal<SidebarFolder[]> = signal([]);
 
-  constructor(private http: HttpClient, private dropDownService: dropDownService, private toast: ToastService) { }
+  constructor(
+    private http: HttpClient,
+    private dropDownService: dropDownService,
+    private toast: ToastService
+  ) {}
 
   // Folders
 
   fetchFolder() {
-    this.http.get<SidebarFolderArrayResponse>(`${APP_URL}/folder`).pipe(
-      map((res: SidebarFolderArrayResponse) => res.data),
-    ).subscribe((Folders: SidebarFolder[]) => {
-      this.sidebarFolders.set(Folders);
-    });
+    this.http
+      .get<SidebarFolderArrayResponse>(`${APP_URL}/folder`)
+      .pipe(map((res: SidebarFolderArrayResponse) => res.data))
+      .subscribe((Folders: SidebarFolder[]) => {
+        this.sidebarFolders.set(Folders);
+      });
   }
 
   getFolder() {
@@ -41,57 +49,60 @@ export class sidebarFolderService {
       next: (res: SidebarFolderResponse) => {
         const sidebarFolder = res.data;
         this.sidebarFolders().push(sidebarFolder);
-
       },
       error: (error) => {
         const err = error.error.message;
         if (!err) {
-          this.toast.error("Check connection.");
-          return
+          this.toast.error('Check connection.');
+          return;
         }
-        this.toast.error(err)
-      }
-    })
+        this.toast.error(err);
+      },
+    });
   }
 
-
   updateFolder(id: number, option: UpdateFolderBody) {
-    this.http.patch<SidebarFolderArrayResponse>(`${APP_URL}/folder/${id}`, option).subscribe({
-      next: () => {
-        this.sidebarFolders().map((sidebarFolder: SidebarFolder) => {
-          if (id === sidebarFolder.id) sidebarFolder.name = option.name
-        })
-        this.toast.success('Rename folder sucessfully.')
-      },
-      error: (error) => {
-        const err = error.error.message;
-        if (!err) {
-          this.toast.error("Check connection.");
-          return
-        }
-        this.toast.error(err)
-      }
-    })
-  };
-
+    this.http
+      .patch<SidebarFolderArrayResponse>(`${APP_URL}/folder/${id}`, option)
+      .subscribe({
+        next: () => {
+          this.sidebarFolders().map((sidebarFolder: SidebarFolder) => {
+            if (id === sidebarFolder.id) sidebarFolder.name = option.name;
+          });
+          this.toast.success('Rename folder sucessfully.');
+        },
+        error: (error) => {
+          const err = error.error.message;
+          if (!err) {
+            this.toast.error('Check connection.');
+            return;
+          }
+          this.toast.error(err);
+        },
+      });
+  }
 
   deleteFolder(id: number) {
-    this.http.delete<SidebarFolderArrayResponse>(`${APP_URL}/folder/${id}`).subscribe({
-      next: () => {
-        let removedData = this.sidebarFolders().filter((sidebarFolder: SidebarFolder) => sidebarFolder.id !== id)
-        this.sidebarFolders.set(removedData)
+    this.http
+      .delete<SidebarFolderArrayResponse>(`${APP_URL}/folder/${id}`)
+      .subscribe({
+        next: () => {
+          let removedData = this.sidebarFolders().filter(
+            (sidebarFolder: SidebarFolder) => sidebarFolder.id !== id
+          );
+          this.sidebarFolders.set(removedData);
 
-        this.toast.success('Folder deleted sucessfully.')
-      },
-      error: (error) => {
-        const err = error.error.message;
-        if (!err) {
-          this.toast.error("Check connection.");
-          return
-        }
-        this.toast.error(err)
-      }
-    })
+          this.toast.success('Folder deleted sucessfully.');
+        },
+        error: (error) => {
+          const err = error.error.message;
+          if (!err) {
+            this.toast.error('Check connection.');
+            return;
+          }
+          this.toast.error(err);
+        },
+      });
   }
 
   getFolderImage(id: number) {
@@ -99,11 +110,11 @@ export class sidebarFolderService {
   }
 
   populateSearchResult(searchText: string) {
-    let filterData = this.sidebarFolders().filter(folder => {
+    let filterData = this.sidebarFolders().filter((folder) => {
       const folderTitle = folder.name.toLocaleLowerCase();
       return folderTitle.includes(searchText.toLowerCase());
-    })
-    this.searchResult.set(filterData)
+    });
+    this.searchResult.set(filterData);
   }
 
   sidebarFolderSearchResult() {
@@ -111,18 +122,20 @@ export class sidebarFolderService {
   }
 
   isSearchEmpty() {
-    return (this.searchResult().length == 0) ? 1 : 0;
+    return this.searchResult().length == 0 ? 1 : 0;
   }
 
   setFolderActive(folder_id: number) {
     const sidebarFoldersArr = this.sidebarFolders();
-    const activeFolder = sidebarFoldersArr.find((item: SidebarFolder) => item.id === folder_id);
+    const activeFolder = sidebarFoldersArr.find(
+      (item: SidebarFolder) => item.id === folder_id
+    );
 
     sidebarFoldersArr.forEach((item: SidebarFolder) => {
       item.active = item === activeFolder;
     });
 
-    this.dropDownService.setFilterType('Date')
+    this.dropDownService.setFilterType('Date');
   }
 
   clearActiveFolder() {
@@ -132,6 +145,6 @@ export class sidebarFolderService {
       item.active = false;
     });
 
-    this.sidebarFolders.set(sidebarFoldersArr)
+    this.sidebarFolders.set(sidebarFoldersArr);
   }
 }
