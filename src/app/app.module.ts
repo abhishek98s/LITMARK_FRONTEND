754e3a64580ab3aac4ecd4d0f164070b2f415082
abhishek98s <abhishekshakya98.com@gmail.com>
2025-02-17 16:59:51 +0545

    remove: unsed import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  HTTP_INTERCEPTORS,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
@@ -9,8 +8,7 @@ import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { Meta } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 
 import { ToastrModule } from 'ngx-toastr';
@@ -19,13 +17,8 @@ import { AuthGuard } from './guard/auth.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecentBoomkarkPageComponent } from './modules/bookmark/recent-boomkark-page/recent-boomkark-page.component';
-import { LayoutComponent } from './modules/layout/layout/layout.component';
-import { BookmarkModule } from './modules/bookmark/bookmark.module';
 
 import { SharedModule } from './shared/shared.module';
-import { LoadingComponent } from './modules/layout/loading/loading.component';
-import { SettingsModule } from './modules/settings/settings.module';
 import { RegisterModule } from './modules/register/register.module';
 import { LoginModule } from './modules/login/login.module';
 
@@ -36,9 +29,9 @@ import { LoginModule } from './modules/login/login.module';
   imports: [
     JwtModule.forRoot({
       config: {
-        tokenGetter: () => localStorage.getItem('token'), // Provide a function to retrieve the token from storage
-        allowedDomains: ['http://localhost:4200/'], // Specify the domains where the token should be sent
-        disallowedRoutes: ['example.com/auth'], // Specify routes that don't need the token
+        tokenGetter: () => localStorage.getItem('token'),
+        allowedDomains: ['http://localhost:4200/'],
+        disallowedRoutes: ['example.com/auth'],
       },
     }),
     BrowserModule,
